530b859d06bc65532e9c5ffd72a2f67c11c50eb6
markb <markb@MarkAnthonysPc>
2025-02-28 12:41:38 +1000

    [Copied from laptop]

--- src/main/java/com/example/tictactoe/TicTacToeGUI.java
+++ src/main/java/com/example/tictactoe/TicTacToeGUI.java
@@ -7,13 +7,14 @@ import javafx.stage.Stage;
 
 public class TicTacToeGUI extends Tic_Tac_Toe1 {
 
+    // Create buttons for each cell of the grid
+    Button[][] buttons = new Button[3][3];
+
     @Override
     public void start(Stage primaryStage) {
          //Create a new grid for the Tic-Tac-Toe board
         GridPane grid = new GridPane();
 
-        // Create buttons for each cell of the grid
-        Button[][] buttons = new Button[3][3];
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -32,16 +33,42 @@ public class TicTacToeGUI extends Tic_Tac_Toe1 {
         primaryStage.show();
     }
 
+    public void resetButtons(){
+         clearBoard();
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText(" ");
+            }
+    }
+
 
     private void handleMove(Button button, int row, int col) {
         if (button.getText().equals(" ")) {
             putMark(row, col);
             button.setText(player == X ? "O" : "X");  // Display O or X on the button
             int winner = winner();  // Check if there's a winner
-            if (winner != 0) {
+
+            //================MODIFIED========================
+
+            if (winner == X) {
               //Displays winner
-                System.out.println("Player " + (winner == X ? "X" : "O") + " wins!");
+                System.out.println("Player " + (winner == X ? "X" : "O") + " wins!\n");
+                addScore("Player X", 100);
+                System.out.println("=====SCOREBOARD=====");
+                displayScoreboard();
+                resetButtons();
+
+
+                }else if (winner == O) {
+                System.out.println("Player " + (winner == O ? "O" : "X") + " wins\n");
+                addScore("Player O", 100);
+                System.out.println("=====SCOREBOARD=====");
+                displayScoreboard();
+                resetButtons();
+
             }
+
+
         }
     }
 }
